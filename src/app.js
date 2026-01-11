a2a4d94b52992b9206152749bacb4cf686aa822b
Diego-Tello <diegotellomendoza@gmail.com>
2026-01-11 23:49:48 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,37 @@
-import "bootstrap";
-import "./style.css";
+window.onload = () => {
+
+let pronoun = ['the', 'our', 'this', 'my', 'every'];
+let adj = ['great', 'big', 'fast', 'brave', 'lucky'];
+let noun = ['jogger', 'racoon', 'planet', 'rocket', 'ninja'];
+let ext = ['.com', '.net', '.eu', '.us', '.pe']
+
+const btnGenerater = document.querySelector('.btn_generador');
+
+btnGenerater.addEventListener("click", ()=>{
+
+function getNumb(arr) {
+  return Math.floor(Math.random() * arr.length)
+}
+
+let getPronoun = pronoun[getNumb(pronoun)];
+let getAdj = adj[getNumb(adj)];
+let getNoun = noun[getNumb(noun)];
+let getExt = ext[getNumb(ext)];
+
+let domGenerated = getPronoun + getAdj + getNoun + getExt;
+console.log("Este es el Dominio: " + domGenerated)
+document.querySelector('.domain').innerHTML=domGenerated;
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let nounCut = getNoun.slice(0, -2)
+let nounExt = getNoun.slice(-2)
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let domHack = getPronoun + getAdj + nounCut + "." + nounExt;
+console.log("Este es el Domain Hack: " + domHack)
+document.querySelector('.hackDomain').innerHTML=domHack;
+
+
+
+})
+
+}
